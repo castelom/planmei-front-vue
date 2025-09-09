3787fd1c5772ec171e7b63d7f0893b0cd86af060
Joao Martins <vitorcastelo.m@gmail.com>
2025-09-09 05:52:02 -0300

    Create alert bar

---
+++ src/components/shared/AlertBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="alert alert-warning d-flex align-items-center" role="alert">
+    <i class="bi bi-exclamation-triangle-fill me-2"></i>
+    <div>
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlertBar",
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
